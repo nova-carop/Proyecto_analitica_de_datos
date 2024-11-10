f729182969b11f404423034a8b8b76bd40804208
mfernandapr <mf-perdomo@hotmail.com>
2024-11-10 19:08:00 -0300

    Update Limpieza_Visualizacion.py

--- Limpieza_Visualizacion.py
+++ Limpieza_Visualizacion.py
@@ -1,217 +1,185 @@
-# %%
-# importar librerias
+# %% importar librerias
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from funpymodeling.exploratory import status
-import numpy as np
+from ydata_profiling import ProfileReport
+import numpy as np                           
 
-# cargar el dataframe con la info del csv de manera generica
+# %% cargar el dataframe con la info del csv de manera generica
 df = pd.read_csv('Tema_10.csv')
 
-# %%
-# analizar info general del df
+# %% [[[[ANALISIS EXPLORATORIO DEL DATAFRAME]]]]
 df.head()
 df.shape
 status(df)
-# %%
-# identificar las filas que estan duplicadas en todas las columnas
-df_duplicados = df[df.duplicated()]
-df_duplicados
-df_duplicados.shape
+# ejecutar el Profile Report
+profile = ProfileReport(df, title="Reporte_Tema10", explorative=True)
+profile.to_file(output_file="Profiling.html")
 
-# eliminar filas duplicadas en el DataFrame original
+
+
+# %% [[[[VISUALIZACIONES]]]]
+
+
+
+# %% [[[[PRETRATAMIENTO DE DATOS]]]]
+
+# %% DUPLICADOS: eliminar filas duplicadas
 df.drop_duplicates(inplace=True)
-
-# Restablecer el índice
 df.reset_index(drop=True, inplace=True)
-df.shape
-# %%
-##################################### ANALISIS EXPLORATORIO VARIABLES ####################################################
 
-############# DATE
-# convertir a datetime el campo date y los valores vacios asignarles NaT (formato: MM-DD-YYYY)
+# %% VARIABLE DATE: 
+# convertir a datetime (formato: MM-DD-YYYY)
 df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y', errors='coerce')
-print(df['date'].dtypes)
-
-# realizar la interpolacion de la columna date para rellenar los valores faltantes
+# completar valores faltantes - realizar interpolación
 df['date'] = df['date'].interpolate()
+# funcion para ajustar la fecha interpolada en caso de que no coincida con el valor en "day" (nombre del dia)
+def adjust_date(row):
+    if pd.notna(row['day']) and pd.notna(row['date']):
+        interp_day = row['date'].day_name()                        # obtener el dia de la semana de date (que contiene los datos interpolados)
+        #si el dia de la semana no coincide ajustar la fecha en date, comenzando en el dia anterior
+        if interp_day != row['day']:
+            target_day = row['day']
+            adjusted_date = row['date'] - pd.Timedelta(days=1)     #empezar 1 dia antes de la fecha
+            while adjusted_date.day_name() != target_day:
+                adjusted_date += pd.Timedelta(days=1)              #sumar de a 1 dia hasta que la fecha coincida con el valor de "day" (nombre del dia)
+            return adjusted_date
+    return row['date']
+# aplicar la funcion adjust_date
+df['date'] = df.apply(adjust_date, axis=1)
 
-# verificar que no hay fechas vacias
-df[df['date'].isna()]
-
-############# QUARTER
-df['quarter'].value_counts()
-
-# genero la columna dia a partir del campo date
-df['dia'] = df['date'].dt.day
-
-# Generar un boxplot con los dias para cada quarter
-data_boxplot_dias_quarter = df[['quarter','dia']]
-plt.figure(figsize=(8, 6))
-sns.boxplot(x='quarter', y='dia', data=data_boxplot_dias_quarter)
-plt.title('Distribucion dias-quarter')
-plt.xlabel('quarter')
-plt.ylabel('dia')
-plt.show() 
-
-# determinar los dias min y max de cada quarter
-dia_min_q1 = df[df['quarter'] == 'Quarter1']['dia'].min()
-dia_max_q1 = df[df['quarter'] == 'Quarter1']['dia'].max()
-dia_min_q2 = df[df['quarter'] == 'Quarter2']['dia'].min()
-dia_max_q2 = df[df['quarter'] == 'Quarter2']['dia'].max()
-dia_min_q3 = df[df['quarter'] == 'Quarter3']['dia'].min()
-dia_max_q3 = df[df['quarter'] == 'Quarter3']['dia'].max()
-dia_min_q4 = df[df['quarter'] == 'Quarter4']['dia'].min()
-dia_max_q4 = df[df['quarter'] == 'Quarter4']['dia'].max()
-print(f'dia_min_q1   {dia_min_q1}')
-print(f'dia_max_q1   {dia_max_q1}')
-print(f'dia_min_q2   {dia_min_q2}')
-print(f'dia_max_q2   {dia_max_q2}')
-print(f'dia_min_q3   {dia_min_q3}')
-print(f'dia_max_q3   {dia_max_q3}')
-print(f'dia_min_q4   {dia_min_q4}')
-print(f'dia_max_q4   {dia_max_q4}')
-
-# funcion para rellenar los valores NaN en quarter, en base al valor de dia
-def assign_quarter(row):
-    if pd.isna(row['quarter']): # solo aplica si quarter es NaN
-        day=row['dia']
-        if 1 <= day <= 7:
-            return 'Quarter1'
-        elif 8 <= day <= 14:
-            return 'Quarter2'
-        elif 15 <= day <= 21:
-            return 'Quarter3'
-        elif day > 21:
-            return 'Quarter4'
-    return row['quarter']
-
-# aplicar la funcion "assign_quarter" en la columna quarter para cada fila
-df['quarter'] = df.apply(assign_quarter, axis=1)
-
-df['quarter'].value_counts()
-df[df['quarter'].isna()]
-
-# quarter: el mes es dividido en 4 por lo que no tiene sentido el dato Quarter5
-# analizar que fechas contiene Quarter5
-fechas_q5 = df[df['quarter'] == 'Quarter5']['date'].unique()
-fechas_q5 
-
-# funcion para modificar los valores de la columna quarter que figuran con el valor Quarter5
-def modify_quarter5(row):
-    if row['quarter']=='Quarter5': # solo aplica si quarter es Quarter5
-        day=row['dia']
-        if 1 <= day <= 7:
-            return 'Quarter1'
-        elif 8 <= day <= 14:
-            return 'Quarter2'
-        elif 15 <= day <= 21:
-            return 'Quarter3'
-        elif day > 21:
-            return 'Quarter4'
-        elif pd.isna(day):
-            return np.nan
-    return row['quarter']
-
-# aplicar la funcion "modify_quarter5" en la columna quarter para cada fila
-df['quarter'] = df.apply(modify_quarter5, axis=1)
-
-# verificar que solo hay quarter 1 a quarter 5
-df['quarter'].value_counts()
-# verificar que no hay vacios en la columna quarter
-df[df['quarter'].isna()]
-
-# Generar un boxplot con los dias para cada quarter con el ajuste realizado
-data_boxplot_dias_quarter_2 = df[['quarter','dia']]
-plt.figure(figsize=(8, 6))
-sns.boxplot(x='quarter', y='dia', data=data_boxplot_dias_quarter_2)
-plt.title('Distribucion dias-quarter ajustado')
-plt.xlabel('quarter')
-plt.ylabel('dia')
-plt.show()
-
-############# DAY
-df[df['day'].isna()]
-
-# funcion para rellenar los valores NaN en day, en base al valor de date
+# %% VARIABLE DAY: 
+# funcion para rellenar los valores NaN, en base al valor de date
 def assign_day(row):
-    if pd.isna(row['day']) and not pd.isna(row['date']): # solo aplica si day es NaN y date no es NaT
+    if pd.isna(row['day']):              
         return row['date'].day_name()
     return row['day']
-
 # aplicar la funcion assign_day en la columna day para cada fila
 df['day'] = df.apply(assign_day, axis=1)
 
-# verificar si hay vacios
-df[df['day'].isna()]
+# %% VARIABLE QUARTER: 
+# funcion para calcular la semana del mes
+def calcular_semana(fecha):
+    return (fecha.day - 1) // 7 + 1
+# crear la columna semana (equivalente a quarter) aplicando la funcion semana a cada fila segun el dato en 'date'
+df['semana'] = df['date'].apply(calcular_semana)
 
-############# DEPARTMENT
-df['department'].value_counts()
-
-# Elimina los espacios en blanco al principio y al final de la cadena
+# %% VARIABLES TEAM y DEPARTMENT: 
+# en DEPARTMENT elimina los espacios vacios al principio y al final
 df['department'] = df['department'].str.strip()
+# en TEAM cambiar el tipo a "int" y asignar NaN a valores vacios o al valor "invalid_value" 
+df['team'] = pd.to_numeric(df['team'], errors='coerce').astype('Int64')        
+# ordenar el dataframe por fecha, team y department
+df.sort_values(by=['date', 'team', 'department']).reset_index(drop=True)
+# completar los valores faltantes en ambas variables usando ffill y bfill: completar los NaN con el valor mas cercano en cualquier direccion (hacia adelante o atras)
+df['department'] = df['department'].ffill().bfill()
+df['team'] = df['team'].ffill().bfill()
 
-# verificar el cambio anterior
-df['department'].value_counts()
 
-# verificar si hay valores vacios en department
-df[df['department'].isna()].shape
+# %% VARIABLE TARGETED_PRODUCTIVITY
 
-############# TEAM
-df['team'].value_counts()
 
-# cambiar el tipo a "int" y asignar NaN a valores vacios o al valor "invalid_value" 
-df['team'] = pd.to_numeric(df['team'], errors='coerce').astype('Int64') 
+# %% VARIABLE SMV
 
-# verificar el cambio anterior y analizar los valores de team
-df['team'].value_counts().sort_index()
 
-# verificar si hay valores vacios en team
-df[df['team'].isna()].shape
+# %% VARIABLE NO_OF_STYLE_CHANGE
 
-# Crear boxplot de team-department
-data_boxplot_team_dep = df[['department','team']]
-plt.figure(figsize=(8, 6))
-sns.boxplot(x='department', y='team', data=data_boxplot_team_dep)
-plt.title('Distribucion team-department')
-plt.xlabel('Department')
-plt.ylabel('Team')
-plt.show()
 
-# PENDIENTE VER COMO COMPLETAR INTERPOLAR VALORES VACIOS EN DEPARTMENT Y TEAM
+# %% VARIABLE NO_OF_WORKERS
 
-# %%
 
-# LIMPIEZA AVANZADA PARA LOS CAMPOS 10 A 15
-
-# Campo 10: wip
+# %% VARIABLE WIP
 df['wip'] = df['wip'].fillna(df['wip'].median())  # Rellenar valores faltantes con la mediana
 df['wip'] = df['wip'].apply(lambda x: min(max(x, 0), 5000))  # Limitar a un rango máximo de 5000 
 
-# Campo 11: over_time
+# %% VARIABLE OVER_TIME
 df['over_time'] = df['over_time'].fillna(0)
 df['over_time'] = df['over_time'].apply(lambda x: min(max(x, 0), 720))  # Limitar a un máximo de 720 minutos
 
-# Campo 12: incentive
+# %% VARIABLE INCENTIVE
 df['incentive'] = df['incentive'].fillna(0)
 df['incentive'] = df['incentive'].apply(lambda x: min(max(x, 0), 500))  # Ajuste a un máximo de 500 
 
-# Campo 13: idle_time
+# %% VARIABLE IDLE_TIME
 df['idle_time'] = df['idle_time'].fillna(0)
 df['idle_time'] = df['idle_time'].apply(lambda x: min(max(x, 0), 60))  # Limitar a un máximo de 60 minutos
 
-# Campo 14: idle_men
+# %% VARIABLE IDLE_MEN
 df['idle_men'] = df['idle_men'].fillna(0).astype(int)
 df['idle_men'] = df['idle_men'].apply(lambda x: min(max(x, 0), 10))  # Limitar a un máximo de 10 personas
 
-# Campo 15: actual_productivity
+# %% VARIABLE ACTUAL_PRODUCTIVITY
 df['actual_productivity'] = df['actual_productivity'].fillna(0)  # Rellenar valores faltantes con 0
 df['actual_productivity'] = df['actual_productivity'].apply(lambda x: min(max(x, 0), 1))  # Limitar al rango [0, 1]
 
-# Vista previa de los datos limpios
-df[['wip', 'over_time', 'incentive', 'idle_time', 'idle_men', 'actual_productivity']].head()
+#%% Verificar que no hay datos faltantes
+df.isnull().sum()
 
 
+
+
+
+
+# %% [[[[VISUALIZACIONES POST LIMPIEZA]]]]
+
+
+
+
+# %% [[[[MODELO PREDICTIVO]]]]
+
+# %% importar librerias
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+# %% aplicar codificación one-hot a las columnas categoricas 'day', 'department'
+df = pd.get_dummies(df, columns=['day','department'])
+
+# %% eliminar columnas innecesarias
+df.drop(['date','quarter','day,'department'], axis=1, inplace=True)
+
+# %% normalizar datos
+def normalizar_datos(data):
+    '''Normalizar datos'''
+    scaler = StandardScaler()
+    data = scaler.fit_transform(data)
+    return data
+df = normalizar_datos(df)
+
+# %% Dividir datos en entrenamiento, validación y prueba
+y = df['actual_productivity']
+X = df.drop('actual_productivity', axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+# %% Entrenar el Modelo Regresión lineal
+regresion_lineal = LinearRegression()
+regresion_lineal.fit(X_train, y_train)
+
+# %% Predicciones
+y_train_pred = regresion_lineal.predict(X_train)
+y_val_pred = regresion_lineal.predict(X_val)
+
+# %% Calcular Metricas de Evaluacion
+# Error cuadrático medio
+print('Error cuadrático medio entrenamiento: ', mean_squared_error(y_train, y_train_pred))
+print('Error cuadrático medio validación: ', mean_squared_error(y_val, y_val_pred))
+# R2
+print('R2 entrenamiento: ', r2_score(y_train, y_train_pred))
+print('R2 validación: ', r2_score(y_val, y_val_pred))
+# Error absoluto medio
+print('Error absoluto medio entrenamiento: ', np.mean(np.abs(y_train - y_train_pred)))
+print('Error absoluto medio validación: ', np.mean(np.abs(y_val - y_val_pred)))
+# Error RMSE
+print('Error RMSE entrenamiento: ', np.sqrt(mean_squared_error(y_train, y_train_pred)))
+print('Error RMSE validación: ', np.sqrt(mean_squared_error(y_val, y_val_pred)))
+# Error absoluto medio porcentual
+print('Error absoluto medio porcentual entrenamiento: ', np.mean(np.abs(y_train - y_train_pred)/y_train))
+print('Error absoluto medio porcentual validación: ', np.mean(np.abs(y_val - y_val_pred)/y_val))
 
 
